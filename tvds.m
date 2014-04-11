9eb7a3867846295f34d55582cd36fbec44b09b5a
Matthew Strickland <matthew@idlefusion.com>
2014-04-11 10:36:18 -0400

    Create tvds.m

---
+++ tvds.m
@@ -0,0 +1,23 @@
+static NSString *eventCellIdentifier = <#identifier#>;
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return <#number#>;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return <#number#>
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+
+    [self configureCell:cell forRowAtIndexPath:indexPath];
+
+    return cell;
+}
+
+- (void)configureCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
+    <#statements#>
+}
