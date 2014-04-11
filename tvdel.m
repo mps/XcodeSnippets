45f4d80cc1476155c78848e7f445e1e7d1c37ea5
Matthew Strickland <matthew@idlefusion.com>
2014-04-11 10:42:32 -0400

    Create tvdel.m

---
+++ tvdel.m
@@ -0,0 +1,5 @@
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	[tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
