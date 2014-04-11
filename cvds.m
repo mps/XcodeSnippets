305b10a13e4a46852de6810b67216d2a39a5eea9
Matthew Strickland <matthew@idlefusion.com>
2014-04-11 11:02:07 -0400

    Create cvds.m

---
+++ cvds.m
@@ -0,0 +1,19 @@
+static NSString *collectionViewCellIdentifer = <#identifer#>;
+
+#pragma mark - UICollectionViewDataSource
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return <#numberOfItemsInSection#>;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:<#collectionViewCellidentifier#> forIndexPath:indexPath];
+	
+    [self configureCell:cell forItemAtIndexPath:indexPath];
+	
+    return cell;
+}
+
+- (void)configureCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath {
+	
+}
